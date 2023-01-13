daebbc0c13aae07739b3df6ef67c84c55552297c
Patrik200414 <martinpatrik14@gmail.com>
2023-01-13 19:50:41 +0100

    Make the delete and the clear button work
    When you click the delete button it will erase the html content
    When you click the clear element it will delete one number from the
    current number

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let state = {
     total: null
 }
 
+document.querySelector('#delBtn').addEventListener('click', () => {
+    document.querySelector('#upper').textContent = '';
+    document.querySelector('#lower').textContent = '';
+});
+
+document.querySelector('#clrBtn').addEventListener('click', () => {
+    let curNum = state.currentNumber.toString();
+    let curNumArr = [...curNum];
+    curNumArr.pop();
+    let rtnNum = Number(curNumArr.join(''));
+    state.currentNumber = rtnNum;
+    console.log(state.currentNumber);
+})
