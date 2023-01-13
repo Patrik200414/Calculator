5bf98457f14ed94e334659d8db235489f818f3c3
Patrik200414 <martinpatrik14@gmail.com>
2023-01-13 20:39:40 +0100

    Make the buttons work
    I ceated an event listener to all of the button elements
    If you click a cumber it will set the stat.currentNumber element to the number
    If you clicked an operator it will set the state.currentOperator element
    to the operator

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let state = {
-    currentNumber : 1,
+    currentNumber : null,
     currentOperator : null,
     upperNum: null,
     total: null
 }
 
+//delete and clear buttons
 document.querySelector('#delBtn').addEventListener('click', () => {
     document.querySelector('#upper').textContent = '';
     document.querySelector('#lower').textContent = '';
@@ -23,5 +24,23 @@ document.querySelector('#clrBtn').addEventListener('click', () => {
     else{
         return;
     }
-    
 })
+
+//numbers and operators
+let buttons = document.querySelectorAll('.btn');
+for(let item of buttons){
+    item.addEventListener('click', (event) => {
+        if( event.target.classList.length !== 2){
+            if(state.currentNumber !== null){
+                state.currentNumber += event.target.value;
+            }
+            else{
+                state.currentNumber = event.target.value;
+            }
+        }
+        else{
+            state.currentOperator = event.target.value;
+        }
+        console.log(state.currentNumber);
+    })
+}
